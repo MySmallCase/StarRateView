baeb1e0b92f9eaa1de8dcfae2c49593d11b4fc8b
向云飞 <978771447@qq.com>
2015-10-27 16:01:25 +0800

    创建项目

---
+++ StarRateView/StarRateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarRateView
+//
+//  Created by MyMac on 15/10/27.
+//  Copyright © 2015年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
